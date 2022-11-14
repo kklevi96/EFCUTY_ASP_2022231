6a949c65f96b6587b927bd4e9490f2b1ae64d23e
kiss.levente <kiss.levente@stud.uni-obuda.hu>
2022-11-14 12:48:06 +0100

    Add project files.

---
+++ EFCUTY_ASP_2022231/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EFCUTY_ASP_2022231.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
