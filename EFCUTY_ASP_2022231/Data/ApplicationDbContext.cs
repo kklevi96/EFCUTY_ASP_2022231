d845ee09be464dc5717b6e8e7072be1229c4037a
kiss.levente <kiss.levente@stud.uni-obuda.hu>
2022-11-19 22:54:40 +0100

    onmodelcreating (no data seeding yet)

--- EFCUTY_ASP_2022231/Data/ApplicationDbContext.cs
+++ EFCUTY_ASP_2022231/Data/ApplicationDbContext.cs
@@ -1,13 +1,49 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EFCUTY_ASP_2022231.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EFCUTY_ASP_2022231.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<SiteUser> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Comment>()
+                .HasOne(t => t.Post)
+                .WithMany()
+                .HasForeignKey(t => t.PostId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Comment>()
+                .HasOne(t => t.Author)
+                .WithMany()
+                .HasForeignKey(t => t.SiteUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Post>()
+                .HasOne(t => t.Subject)
+                .WithMany()
+                .HasForeignKey(t => t.SubjectCode)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Post>()
+                .HasOne(t => t.Author)
+                .WithMany()
+                .HasForeignKey(t => t.SiteUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
         }
     }
 }
