62ce023f51b16821af585f373a175c4e3beac4cd
kiss.levente <kiss.levente@stud.uni-obuda.hu>
2022-11-22 20:39:29 +0100

    Helper classes added to help displaying

---
+++ EFCUTY_ASP_2022231/ViewModels/CommentsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using EFCUTY_ASP_2022231.Models;
+
+namespace EFCUTY_ASP_2022231.ViewModels
+{
+    public class CommentsViewModel
+    {
+        public IEnumerable<Comment> Comments { get; set; }
+        public string PostId { get; set; }
+    }
+}
---
+++ EFCUTY_ASP_2022231/ViewModels/PostsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using EFCUTY_ASP_2022231.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCUTY_ASP_2022231.ViewModels
+{
+    public class PostsViewModel
+    {
+        public IEnumerable<Post> Posts { get; set; }
+        public string SubjectCode { get; set; }
+    }
+}
