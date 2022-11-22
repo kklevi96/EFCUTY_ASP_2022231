d0b7683f848c7b17678bec7bb3642e2f29a627cf
kiss.levente <kiss.levente@stud.uni-obuda.hu>
2022-11-22 20:38:40 +0100

    ModelState.IsValid false bugfix with helper classes

---
+++ EFCUTY_ASP_2022231/ViewModels/CommentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCUTY_ASP_2022231.ViewModels
+{
+    public class CommentViewModel
+    {
+        [Required]
+        [MinLength(10)]
+        [MaxLength(300)]
+        public string Content { get; set; }
+        public string PostId { get; set; }
+    }
+}
---
+++ EFCUTY_ASP_2022231/ViewModels/PostViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EFCUTY_ASP_2022231.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCUTY_ASP_2022231.ViewModels
+{
+    public class PostViewModel
+    {
+        [Required]
+        [MinLength(10)]
+        public string Content { get; set; }
+        public string SubjectCode { get; set; }
+    }
+}
