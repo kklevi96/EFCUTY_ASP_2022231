63b1d431efc2bdeecfda89dbc03a3021a89ce62f
kiss.levente <kiss.levente@stud.uni-obuda.hu>
2022-11-22 21:40:06 +0100

    Register form updated

--- EFCUTY_ASP_2022231/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ EFCUTY_ASP_2022231/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,16 @@ namespace EFCUTY_ASP_2022231.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [StringLength(100)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(100)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -114,7 +124,8 @@ namespace EFCUTY_ASP_2022231.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
-
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
