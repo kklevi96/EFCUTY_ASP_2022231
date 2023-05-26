4ed7364722480f2770c1c497d2f3c67aef325a5c
kklevi96 <kklevi@gmail.com>
2023-05-26 17:46:21 +0200

    Subject api endpoint added, updated

---
+++ KF-EFCUTY-2022-2023-2-BPROF.Backend/EFCUTY_ASP_2022231/Controllers/SubjectsApiController.cs
@@ -0,0 +1,110 @@
+﻿using EFCUTY_ASP_2022231.Models;
+using EFCUTY_ASP_2022231.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCUTY_ASP_2022231.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SubjectsApiController : ControllerBase
+    {
+        private readonly ISubjectsRepository repository;
+        private readonly UserManager<SiteUser> userManager;
+
+        public SubjectsApiController(ISubjectsRepository repository, UserManager<SiteUser> userManager)
+        {
+            this.repository = repository;
+            this.userManager = userManager;
+        }
+
+
+
+        // GET: Subjects
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(this.repository.GetAll().OrderBy(s => s.Semester));
+        }
+
+        // GET: Subjects/Details/5
+        [HttpGet("Details/{id}")]
+        public IActionResult Details(string id)
+        {
+            var subject = this.repository.GetOne(id);
+
+            if (id == null || subject == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(subject);
+        }
+
+        // POST: Subjects/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost("Create")]
+        public IActionResult Create(Subject subject)
+        {
+            if (String.IsNullOrEmpty(subject.Name) || String.IsNullOrEmpty(subject.SubjectCode))
+                return BadRequest();
+
+            if (!User.IsInRole("Admin"))
+                return Unauthorized();
+
+            Subject n = new Subject()
+            {
+                Semester = subject.Semester,
+                CreditValue = subject.CreditValue,
+                ExamSubject = subject.ExamSubject,
+                Name = subject.Name,
+                SubjectCode = subject.SubjectCode
+            };
+            this.repository.Create(n);
+            return Ok(n);
+        }
+
+        // POST: Subjects/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost("Edit/{id}")]
+        public IActionResult Edit(string id, Subject subject)
+        {
+            if (!String.IsNullOrEmpty(subject.Name))
+            {
+                var old = this.repository.GetOne(id);
+
+                if (!User.IsInRole("Admin"))
+                {
+                    return Unauthorized();
+                }
+
+                old.Name = subject.Name;
+                old.Semester = subject.Semester;
+                old.ExamSubject = subject.ExamSubject;
+                old.CreditValue = subject.CreditValue;
+                this.repository.Update(old);
+
+                return Ok(old);
+            }
+            return BadRequest(subject);
+        }
+
+        // GET: Subjects/Delete/5
+        [HttpDelete("Delete")]
+        public IActionResult Delete(string id)
+        {
+            var subject = this.repository.GetOne(id);
+
+            if (subject != null && User.IsInRole("Admin"))
+            {
+                this.repository.Delete(subject);
+                return Ok(subject);
+            }
+
+            return Unauthorized();
+        }
+    }
+}
