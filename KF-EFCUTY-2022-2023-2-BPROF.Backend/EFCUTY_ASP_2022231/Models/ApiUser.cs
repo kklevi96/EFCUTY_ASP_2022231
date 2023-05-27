bdecacbd58a552fea1c2903d5221507dd90022ed
kklevi96 <kklevi@gmail.com>
2023-05-28 00:57:13 +0200

    apiuser model created

---
+++ KF-EFCUTY-2022-2023-2-BPROF.Backend/EFCUTY_ASP_2022231/Models/ApiUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EFCUTY_ASP_2022231.Models
+{
+    public class ApiUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
