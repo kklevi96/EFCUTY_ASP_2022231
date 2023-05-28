258182f51c51f3ca0d09c96f82c6c666259fbd0a
kklevi96 <kklevi@gmail.com>
2023-05-28 02:47:57 +0200

    AuthController added

---
+++ KF-EFCUTY-2022-2023-2-BPROF.Backend/EFCUTY_ASP_2022231/Controllers/AuthController.cs
@@ -0,0 +1,106 @@
+﻿using EFCUTY_ASP_2022231.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace EFCUTY_ASP_2022231.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApiUser> _userManager;
+        private readonly Microsoft.Extensions.Configuration.IConfiguration _configuration;
+        public AuthController(UserManager<ApiUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var claim = new List<Claim> { new Claim(JwtRegisteredClaimNames.Sub, user.UserName) };
+                foreach (var role in await _userManager.GetRolesAsync(user))
+                {
+                    claim.Add(new Claim(ClaimTypes.Role, role));
+                }
+                var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("nagyonhosszutitkoskodhelye"));
+                var token = new JwtSecurityToken(
+                issuer: "http://www.security.org", audience: "http://www.security.org",
+                claims: claim, expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> InsertUser([FromBody] RegisterViewModel model)
+        {
+            var user = new ApiUser
+            {
+                Email = model.Email,
+                UserName = model.UserName,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+            };
+            await _userManager.CreateAsync(user, model.Password);
+            await _userManager.AddToRoleAsync(user, "User");
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetUserInfos()
+        {
+            var user = _userManager.Users.FirstOrDefault(t => t.Email == this.User.Identity.Name);
+            return Ok(new
+            {
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Roles = await _userManager.GetRolesAsync(user)
+            });
+        }
+
+        [Authorize]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteMyself()
+        {
+            var user = _userManager.Users.FirstOrDefault(t => t.Email == this.User.Identity.Name);
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> UpdateProfile([FromBody] RegisterViewModel model)
+        {
+            var user = _userManager.Users.FirstOrDefault(t => t.Email == this.User.Identity.Name);
+            user.Email = model.Email;
+            user.UserName = model.UserName;
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            await _userManager.UpdateAsync(user);
+            return Ok();
+        }
+    }
+}
