ee8fc90cd9d12c08cbfc6c32d671feab68ad8367
kklevi96 <kklevi@gmail.com>
2023-05-28 02:47:44 +0200

    Testing

--- KF-EFCUTY-2022-2023-2-BPROF.Backend/EFCUTY_ASP_2022231/Controllers/HomeController.cs
+++ KF-EFCUTY-2022-2023-2-BPROF.Backend/EFCUTY_ASP_2022231/Controllers/HomeController.cs
@@ -1,78 +1,78 @@
-﻿//using EFCUTY_ASP_2022231.Models;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
-//using System.Diagnostics;
+﻿using EFCUTY_ASP_2022231.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
-//namespace EFCUTY_ASP_2022231.Controllers
-//{
-//    public class HomeController : Controller
-//    {
-//        private readonly ILogger<HomeController> _logger;
-//        private readonly UserManager<SiteUser> _userManager;
-//        private readonly RoleManager<IdentityRole> _roleManager;
-
-
-//        public HomeController(ILogger<HomeController> logger, UserManager<SiteUser> userManager, RoleManager<IdentityRole> roleManager)
-//        {
-//            _logger = logger;
-//            _userManager = userManager;
-//            _roleManager = roleManager;
-//        }
-
-//        public IActionResult Index()
-//        {
-//            return RedirectToAction("Index", "Subjects");
-//        }
-
-//        public IActionResult Privacy()
-//        {
-//            return View();
-//        }
-
-//        public async Task<IActionResult> DelegateAdmin()
-//        {
-//            var principal = this.User;
-//            var user = await _userManager.GetUserAsync(principal);
-//            var role = new IdentityRole()
-//            {
-//                Name = "Admin"
-//            };
-//            if (!await _roleManager.RoleExistsAsync("Admin"))
-//            {
-//                await _roleManager.CreateAsync(role);
-//            }
-//            await _userManager.AddToRoleAsync(user, "Admin");
-//            return RedirectToAction(nameof(Index));
-//        }
-
-//        [Authorize(Roles = "Admin")]
-//        public async Task<IActionResult> RemoveAdmin(string uid)
-//        {
-//            var user = _userManager.Users.FirstOrDefault(t => t.Id == uid);
-//            await _userManager.RemoveFromRoleAsync(user, "Admin");
-//            return RedirectToAction(nameof(Admin));
-//        }
-
-//        [Authorize(Roles = "Admin")]
-//        public async Task<IActionResult> GrantAdmin(string uid)
-//        {
-//            var user = _userManager.Users.FirstOrDefault(t => t.Id == uid);
-//            await _userManager.AddToRoleAsync(user, "Admin");
-//            return RedirectToAction(nameof(Admin));
-//        }
+namespace EFCUTY_ASP_2022231.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<SiteUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
 
-//        [Authorize(Roles = "Admin")]
-//        public IActionResult Admin()
-//        {
-//            return View(_userManager.Users);
-//        }
+        public HomeController(ILogger<HomeController> logger, UserManager<SiteUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
 
-//        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-//        public IActionResult Error()
-//        {
-//            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-//        }
-//    }
-//}
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Subjects");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> DelegateAdmin()
+        {
+            var principal = this.User;
+            var user = await _userManager.GetUserAsync(principal);
+            var role = new IdentityRole()
+            {
+                Name = "Admin"
+            };
+            if (!await _roleManager.RoleExistsAsync("Admin"))
+            {
+                await _roleManager.CreateAsync(role);
+            }
+            await _userManager.AddToRoleAsync(user, "Admin");
+            return RedirectToAction(nameof(Index));
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> RemoveAdmin(string uid)
+        {
+            var user = _userManager.Users.FirstOrDefault(t => t.Id == uid);
+            await _userManager.RemoveFromRoleAsync(user, "Admin");
+            return RedirectToAction(nameof(Admin));
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GrantAdmin(string uid)
+        {
+            var user = _userManager.Users.FirstOrDefault(t => t.Id == uid);
+            await _userManager.AddToRoleAsync(user, "Admin");
+            return RedirectToAction(nameof(Admin));
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Admin()
+        {
+            return View(_userManager.Users);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
