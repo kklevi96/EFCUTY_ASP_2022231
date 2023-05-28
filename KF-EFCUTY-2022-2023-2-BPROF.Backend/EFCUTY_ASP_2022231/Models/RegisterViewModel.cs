268187aa91e238228f5adfeee46cf0a1c3e61a47
kklevi96 <kklevi@gmail.com>
2023-05-28 02:48:38 +0200

    add register viewmodel

---
+++ KF-EFCUTY-2022-2023-2-BPROF.Backend/EFCUTY_ASP_2022231/Models/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCUTY_ASP_2022231.Models
+{
+    public class RegisterViewModel
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
